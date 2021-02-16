b1598771ab4e261a558ca47d79adc9d71cbe6d5a
Ben Munro <bmunro@rossvideo.com>
2021-02-16 10:14:03 -0500

    it works
    updating to react-flow 9 and adding ResizeObserver allows the demo to
    run in IE

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'core-js/stable';
-import 'proxy-polyfill/proxy.min.js';
-import { enableES5 } from 'immer';
-enableES5();
+import 'resize-observer-polyfill/dist/ResizeObserver.global';
 
 ReactDOM.render(
   <React.StrictMode>
